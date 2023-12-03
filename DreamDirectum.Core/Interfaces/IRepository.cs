3322b1c94375f26b9cc67c451c0ad303e3146187
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-12-03 23:32:06 +0500

    created simple IRepository interface and AbstractRepository class

---
+++ DreamDirectum.Core/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamDirectum.Core.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> AddAsync(T entity, CancellationToken cancellationToken = default);
+        Task<IEnumerable<T>> GetAllAsync( CancellationToken cancellationToken = default);
+        Task<T?> GetByIdAsync<TId>(TId id, CancellationToken cancellationToken = default) where TId : notnull;
+    }
+}
---
+++ DreamDirectum.Infrastructure/Repositories/AbstractRepository.cs
@@ -0,0 +1,14 @@
+﻿using Sungero.IntegrationService;
+
+namespace DreamDirectum.Infrastructure.Repositories
+{
+    public abstract class AbstractRepository
+    {
+        protected readonly Container container;
+
+        protected AbstractRepository(Container container)
+        {
+            this.container = container;
+        }
+    }
+}
