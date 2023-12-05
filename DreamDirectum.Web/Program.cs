f1ce40e9f5657aa5ff68c074c5f2b0294b279535
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-12-05 18:58:08 +0500

    updated di services registration

--- DreamDirectum.Web/Program.cs
+++ DreamDirectum.Web/Program.cs
@@ -1,4 +1,14 @@
 
+using DreamDirectum.Core.Interfaces;
+using DreamDirectum.Core.Models.Configuration;
+using DreamDirectum.Core.Services;
+using DreamDirectum.Infrastructure.Repositories;
+using DreamDirectum.UseCases.Queries.EmployeeQueries;
+using Microsoft.Extensions.Configuration;
+using Sungero.IntegrationService;
+using System.Net.NetworkInformation;
+using System.Reflection;
+
 namespace DreamDirectum.Web
 {
     public class Program
@@ -7,12 +17,19 @@ namespace DreamDirectum.Web
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            var directumConfiguration = builder.Configuration.GetSection("Directum");
+            builder.Services.Configure<DirectumConfiguration>(directumConfiguration);
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<GetAllEmployeesQuery>());
+
+            builder.Services.AddScoped<IUserAuthTokenService, UserAuthTokenService>();
+            builder.Services.AddScoped<EmployeeRepository>();
+            builder.Services.AddScoped<EmployeeMutationKindRepository>();
+            builder.Services.AddScoped((sp) => new Container(new Uri("https://drim-student.starkovgrp.ru/Integration/odata/")));
 
             var app = builder.Build();
 
