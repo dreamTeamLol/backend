9ed4a59f0f4ac80741cd4625441380bbb2387fc8
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-12-05 18:50:20 +0500

    created EmployeeMutationKindRepository

---
+++ DreamDirectum.Infrastructure/Repositories/EmployeeMutationKindRepository.cs
@@ -0,0 +1,33 @@
+﻿using DreamDirectum.Core.Interfaces;
+using Sungero.IntegrationService;
+using Sungero.IntegrationService.Models.Generated.EmployeeModule;
+
+namespace DreamDirectum.Infrastructure.Repositories
+{
+    public class EmployeeMutationKindRepository : AbstractRepository, IReadOnlyRepository<IEmployeeMutationKindDto, long>
+    {
+        public EmployeeMutationKindRepository(Container container) : base(container)
+        { }
+
+        public Task<IEnumerable<IEmployeeMutationKindDto>> GetAllAsync(string authToken, CancellationToken cancellationToken = default)
+        {
+            SetAuthorizationHeader(authToken);
+            return Task.FromResult(container.IEmployeeMutationKinds.AsEnumerable());
+        }
+
+        public Task<IEnumerable<IEmployeeMutationKindDto>> GetAllWithPaginationAsync(string authToken, int limit, int offset, CancellationToken cancellationToken = default)
+        {
+            SetAuthorizationHeader(authToken);
+            return Task.FromResult(container.IEmployeeMutationKinds
+                .AddQueryOption("$top", limit)
+                .AddQueryOption("$skip", offset)
+                .AsEnumerable());
+        }
+
+        public Task<IEmployeeMutationKindDto?> GetByIdAsync(string authToken, long id, CancellationToken cancellationToken = default)
+        {
+            SetAuthorizationHeader(authToken);
+            return Task.FromResult(container.IEmployeeMutationKinds.Where(e => e.Id == id).SingleOrDefault());
+        }
+    }
+}
