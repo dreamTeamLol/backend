0fef4d8ad85d3af96f921e1eaf1256cc162e4b2d
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-12-05 18:53:49 +0500

    created configuration model

---
+++ DreamDirectum.Core/Models/Configuration/DirectumConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace DreamDirectum.Core.Models.Configuration
+{
+    public class DirectumConfiguration
+    {
+        public string BaseUrl { get; set; }
+        public string OdataIntegrationEndpoint { get; set; }
+        public string OdataFullUrl { get => BaseUrl + OdataIntegrationEndpoint; }
+    }
+}
