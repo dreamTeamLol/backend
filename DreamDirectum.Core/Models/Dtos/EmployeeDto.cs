a1f95eabcee451bebadddccea32abeaa8347e5ee
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2023-12-05 18:54:52 +0500

    created dtos

---
+++ DreamDirectum.Core/Models/Dtos/EmployeeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DreamDirectum.Core.Models.Dtos
+{
+    public record EmployeeDto
+    {
+        public long Id { get; set; }
+        public string FullName { get; set; }
+        public string DepartmentName { get; set; }
+        public MutationDto[] Mutations { get; set; }
+    }
+}
---
+++ DreamDirectum.Core/Models/Dtos/MutationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DreamDirectum.Core.Models.Dtos
+{
+    public record MutationDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateTimeOffset? AppliedDate { get; set; }
+        public string Note { get; set; }
+    }
+}
