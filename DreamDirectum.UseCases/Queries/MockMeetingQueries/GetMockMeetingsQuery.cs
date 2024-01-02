7f8c7f5da197d1beded6db239092f97edbe64267
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2024-01-03 01:52:12 +0500

    created mock queries

---
+++ DreamDirectum.UseCases/Queries/MockMeetingQueries/GetMockMeetingsQuery.cs
@@ -0,0 +1,36 @@
+﻿using DreamDirectum.Core.Models.Dtos;
+using MediatR;
+
+namespace DreamDirectum.UseCases.Queries.MockMeetingQueries
+{
+    public class GetMockMeetingsQuery : IRequest<MeetingDto[]>
+    { }
+
+    public class GetMockMeetingsQueryHandler : IRequestHandler<GetMockMeetingsQuery, MeetingDto[]>
+    {
+        private readonly MeetingDto[] meetings =
+        {
+            new MeetingDto
+            {
+                Name = "Заседание Координационного совета  по организации защиты прав застрахованных",
+                DisplayName = "Заседание Координационного совета  по организации защиты прав застрахованных",
+                DateTime = new DateTime(2023, 5, 31),
+                Location = "Конференц-зал, 8 этаж",
+                Secretary = new EmployeeDto { FullName = "Киселева С.О." },
+                President = new EmployeeDto { FullName = "Титкова Н.Г." },
+                Members = new EmployeeDto[]
+                {
+                    new EmployeeDto { FullName = "Кальницкий" },
+                    new EmployeeDto { FullName = "Фатихов" },
+                    new EmployeeDto { FullName = "Британова" },
+                },
+                Note = "приглашенные сотрудники СМО по ВКС"
+            }
+        };
+
+        public Task<MeetingDto[]> Handle(GetMockMeetingsQuery request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(meetings);
+        }
+    }
+}
---
+++ DreamDirectum.UseCases/Queries/MockSubstitutionQueries/GetMockSubstitutionsQuery.cs
@@ -0,0 +1,32 @@
+﻿using DreamDirectum.Core.Models.Dtos;
+using MediatR;  
+
+namespace DreamDirectum.UseCases.Queries.MockSubstitutionQueries
+{
+    public class GetMockSubstitutionsQuery : IRequest<SubstitutionDto[]>
+    {
+        public int Limit { get; set; }
+        public int Offset { get; set; }
+    }
+
+    public class GetMockSubstitutionsQueryHandler : IRequestHandler<GetMockSubstitutionsQuery, SubstitutionDto[]>
+    {
+        private readonly SubstitutionDto[] substitutions =
+        {
+            new SubstitutionDto
+            {
+                User = new EmployeeDto { FullName = "Иванов Сергей Петрович", JobTitle = "Начальник отдела КРУ" },
+                Substitue = new EmployeeDto { FullName = "Сидоров Семен Васильевич", JobTitle = "Главный специалист КРУ" },
+                StartDate = new DateTime(2023, 2, 1),
+                EndDate = new DateTime(2023, 2, 23),
+                Comment = "В отпуске"
+            }
+        };
+
+        public Task<SubstitutionDto[]> Handle(GetMockSubstitutionsQuery request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(substitutions);
+        }
+    }
+}
+ 
