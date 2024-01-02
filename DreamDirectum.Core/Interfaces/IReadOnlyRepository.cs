4cc4041a1e15316cfd93cc3618c4341a8905b63a
Lev Kochnev <75497303+thatslk@users.noreply.github.com>
2024-01-03 01:45:31 +0500

    refactored repository interfaces

--- DreamDirectum.Core/Interfaces/IReadOnlyRepository.cs
+++ DreamDirectum.Core/Interfaces/IReadOnlyRepository.cs
@@ -1,17 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DreamDirectum.Core.Interfaces
+﻿namespace DreamDirectum.Core.Interfaces
 {
     public interface IReadOnlyRepository<T, TId> 
         where T : class 
         where TId : notnull
     {
-        Task<IEnumerable<T>> GetAllAsync(string authToken, CancellationToken cancellationToken = default);
-        Task<IEnumerable<T>> GetAllWithPaginationAsync(string authToken, int limit, int offset, CancellationToken cancellationToken = default);
         Task<T?> GetByIdAsync(string authToken, TId id, CancellationToken cancellationToken = default);
+        Task<T?> GetByIdWithSpecifiedOptionsAsync(string authToken, TId id, CancellationToken cancellationToken = default, params (string key, string value)[] options);
+    }
+
+    public interface IReadOnlySinglePageRepository<T, TId>
+        where T : class
+        where TId : notnull
+    {
+        Task<IEnumerable<T>> GetAllAsync(string authToken, CancellationToken cancellationToken = default);
+        Task<IEnumerable<T>> GetAllWithSpecifiedOptionsAsync(string authToken, CancellationToken cancellationToken = default, params (string key, string value)[] options);
+    } 
+
+    public interface IReadOnlyPaginalRepository<T, TId> 
+        where T : class
+        where TId : notnull
+    {
+        Task<IEnumerable<T>> GetAllWithPaginationAsync(string authToken, int limit, int offset, CancellationToken cancellationToken = default);
+        Task<IEnumerable<T>> GetAllWithPaginationAndSpecifiedOptionsAsync(string authToken, int limit, int offset, CancellationToken cancellationToken = default, params (string key, string value)[] options);
     }
 }
